21d26b410e1e8a3759ae29c9e81e9acbc0d78638
arnaumaeso <arnaumaeso@gmail.com>
2024-11-19 18:32:21 +0100

    Update mysqlAddToPokedek.php

--- php/mysqlAddToPokedek.php
+++ php/mysqlAddToPokedek.php
@@ -5,7 +5,7 @@ $id_pokemon = "";
 
 $message = "";
 // Establecemos la conexión con la base de datos
-$link = mysqli_connect("localhost", "root", "", "Pokewebapp");
+$link = mysqli_connect("localhost", "root", "Pa$$w0rd", "Pokewebapp");
 
 // Revisamos que se haya realizado la conexión
 if ($link == false) {
