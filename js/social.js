2ce1dc44faada5948b16825c28a0b21d3a39e6f2
arnaumaeso <arnaumaeso@gmail.com>
2024-11-19 12:57:14 +0100

    Update social.js

--- js/social.js
+++ js/social.js
@@ -13,9 +13,9 @@ function logout() {
 }
 
 function createCookie(name, value, days) {
-  var expires;
+  let expires;
   if (days) {
-    var date = new Date();
+    const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toGMTString();
   } else {
