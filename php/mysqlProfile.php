1c79230aaa62f69e20628315dcae6fdaebf5469c
arnaumaeso <arnaumaeso@gmail.com>
2024-11-19 19:20:16 +0100

    Update mysqlProfile.php

--- php/mysqlProfile.php
+++ php/mysqlProfile.php
@@ -4,7 +4,7 @@ $pokeballs  = 20;
 //establecemos la conexión con la base de datos
 $link = mysqli_connect("localhost","root","Pa$$w0rd","Pokewebapp");
 //revisamos que se haya realizado la conexión
-if($link == false){
+if($link){
 	$message = "ERROR: Could not connect ".mysqli_connect_error();
 }else{
 //obtenemos los datos enviados por el post
