4e8f4fffb05fe6405235e2848eff25c2ab7a5dba
arnaumaeso <arnaumaeso@gmail.com>
2024-11-19 18:34:38 +0100

    Update mysqlSignIn.php

--- php/mysqlSignIn.php
+++ php/mysqlSignIn.php
@@ -2,7 +2,7 @@
 $message = "";
 
 if (!empty($_POST)) {
-    $link = mysqli_connect("localhost","root","","Pokewebapp");
+    $link = mysqli_connect("localhost","root","Pa$$w0rd","Pokewebapp");
 
     if ($link == false) {
         $message = "ERROR: Could not connect " . mysqli_connect_error();
